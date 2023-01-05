7ed0a5ef7e5ca7754f20297455b27d5f70c531a8
clinton otbana <rbnclntn@gmail.com>
2023-01-05 17:14:19 +0800

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { AiOutlineCopyrightCircle } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full text-xs">
+    <footer className="sticky top-[100vh] w-full text-xs mt-20">
       <div className="px-10 lg:px-40 py-5 flex justify-center align-center">
         <span className="flex opacity-75">
           Copyright <AiOutlineCopyrightCircle /> 2023 Clinton Orbaña - All
